3ffc4abcc9dc6aab2afa9247a4b8c502fbdf0192
pvgomes <pv.gomes89@gmail.com>
2017-10-03 16:37:28 -0300

    add help test

---
+++ src/tests.py
@@ -0,0 +1,32 @@
+import commands
+import unittest
+
+
+# Here's our "unit".
+def IsOdd(n):
+    return n % 2 == 1
+
+# Here's our "unit tests".
+class IsOddTests(unittest.TestCase):
+
+    def testOne(self):
+        self.failUnless(IsOdd(1))
+
+    def testTwo(self):
+        self.failIf(IsOdd(2))
+
+
+class HelpTests(unittest.TestCase):
+
+    def testHelper(self):
+        helpCommand = commands.Help()
+        expected = '''
+            This is the help
+        '''
+        self.assertEqual(expected, helpCommand.execute(), "help msg is not the same")
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
